8b79a15348ff33b6ee4b562c5c3a30b31f8f774e
Duduelc7 <eduardocordeiro1906@gmail.com>
2024-08-20 10:00:49 -0300

    cfg_final+readme

--- config_ambiente.ps1
+++ config_ambiente.ps1
@@ -1,24 +1,20 @@
-# Definir a variÃ¡vel de ambiente JAVA_HOME
 $javaHome = "C:\Program Files\Java\jre1.8.0_421"
 [System.Environment]::SetEnvironmentVariable("JAVA_HOME", $javaHome, [System.EnvironmentVariableTarget]::Machine)
 [System.Environment]::SetEnvironmentVariable("Path", $env:Path + ";$javaHome\bin", [System.EnvironmentVariableTarget]::Machine)
-Write-Host "VariÃ¡vel de ambiente JAVA_HOME configurada para $javaHome"
+Write-Host "Variavel de ambiente JAVA_HOME configurada para $javaHome"
 
-# FunÃ§Ã£o para criar o database e executar o script SQL usando psql
 function Create-DatabaseAndTables {
     param (
-        [string]$dbName = "staging_educ_asp",
+        [string]$dbName = "staging_teste",
         [string]$dbUser = "postgres",
         [string]$dbPassword = "postgres",
-        [string]$sqlFilePath = "C:\AS\scr_setup\sql_adventista.sql"
+        [string]$sqlFilePath = "C:\Users\GABRIELA\Desktop\setup_auto\sql_teste.sql"
     )
 
-    # Criar o database usando psql
     Write-Host "Criando database $dbName..."
     $createDbCommand = "CREATE DATABASE $dbName;"
     $createDbCommand | psql -U $dbUser -h localhost -c $createDbCommand
 
-    # Executar o script SQL para criar as tabelas
     Write-Host "Criando tabelas no database $dbName..."
     $psqlCommand = "psql -U $dbUser -d $dbName -h localhost -f $sqlFilePath"
     cmd.exe /c $psqlCommand
@@ -26,5 +22,4 @@ function Create-DatabaseAndTables {
     Write-Host "Tabelas criadas com sucesso no database $dbName."
 }
 
-# Criar o database e tabelas
 Create-DatabaseAndTables
